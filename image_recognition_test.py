a8b8f3555aa2e24db5f800e91524f4fe120f94ff
nazish-qamar <nazish-qamar@users.noreply.github.com>
2021-08-29 23:11:13 +0200

    adding train and test python files

---
+++ image_recognition_test.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 29 18:34:54 2021
+
+@author: nazish
+"""
+
+# # For Genrating test images
+# from PIL import Image
+# from tensorflow.keras.datasets import mnist
+# import numpy as np
+
+# (X_train, y_train), (X_test, y_test) = mnist.load_data()
+# for i in np.random.randint(0, 10000+1, 10):
+#     arr2im = Image.fromarray(X_train[i])
+#     arr2im.save('test_images/{}.png'.format(i), "PNG")
+
+
+from tensorflow.keras.models import load_model
+from PIL import Image
+import numpy as np
+from flasgger import Swagger
+from flask import Flask, request
+app = Flask(__name__)
+swagger = Swagger(app)
+
+    
+model = load_model('./model.h5')
+
+@app.route('/predict_digit', methods=['POST'])
+def predict_digit():
+    """Example endpoint returning a prediction of mnist
+    ---
+    parameters:
+        - name: image
+          in: formData
+          type: file
+          required: true
+    responses:
+        200:
+            description: "text"
+    """
+    im = Image.open(request.files['image'])
+    im2arr = np.array(im).reshape((1, 28, 28, 1))
+    return str(np.argmax(model.predict(im2arr)))
+
+if __name__ == '__main__':
+    app.run()
--- image_recognition_train.py
+++ image_recognition_train.py
@@ -5,6 +5,15 @@ Created on Sun Aug 29 12:54:23 2021
 
 @author: nazish
 """
+# # For Genrating test images
+# from PIL import Image
+# from tensorflow.keras.datasets import mnist
+# import numpy as np
+
+# (X_train, y_train), (X_test, y_test) = mnist.load_data()
+# for i in np.random.randint(0, 10000+1, 10):
+#     arr2im = Image.fromarray(X_train[i])
+#     arr2im.save('test_images/{}.png'.format(i), "PNG")
 
 from tensorflow.keras.datasets import mnist
 import matplotlib. pyplot as plt
@@ -66,6 +75,7 @@ def baseline_model():
 model = baseline_model()
 
 # Fit 
+#print(X_test.shape)
 model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=3,
           batch_size=32, verbose=2)
 
@@ -74,30 +84,6 @@ model.save('model.h5')
 # Final eval
 scores = model.evaluate(X_test, y_test, verbose=0)
 print("CNN error: %.2f%%" % (100 - scores[1]*100))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
